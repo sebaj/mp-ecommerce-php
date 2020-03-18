1cdc2504b568ada7d3c718e56d35e727813d2e27
Sebastian Jaimovich <sebaj80@gmail.com>
2020-03-18 17:23:56 -0300

    pruebas

--- success.php
+++ success.php
@@ -1,3 +1,14 @@
+<?php
+require __DIR__  . '/vendor/autoload.php';
+//$mp = new MercadoPago\SDK();
+MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398");
+
+$response = MercadoPago\SDK::get("https://api.mercadopago.com/v1/payments/6101392552");
+
+var_dump($response);
+die();
+ ?>
+
 <!DOCTYPE html>
 <html class="supports-animation supports-columns svg no-touch no-ie no-oldie no-ios supports-backdrop-filter as-mouseuser" lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
