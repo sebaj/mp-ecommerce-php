20f739346262c6008b19674810a8075c74ff6dec
Sebastián Jaimovich <sebaj80@gmail.com>
2020-05-06 19:10:01 -0300

    fix token key

--- success.php
+++ success.php
@@ -1,6 +1,6 @@
 <?php
 require __DIR__  . '/vendor/autoload.php';
-MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398");
+MercadoPago\SDK::setAccessToken("APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398");
 $mpResponse = MercadoPago\SDK::get( "/v1/payments/". $_GET["collection_id"] );
 ?>
 
